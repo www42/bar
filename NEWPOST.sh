9d671b43d9e2ccf3bff39f8fa25f01b3ed01ef6b
www42 <thomas.jaekel@gfn.de>
2021-01-30 22:39:15 +0100

    author added

---
+++ NEWPOST.sh
@@ -0,0 +1,28 @@
+# -------------
+# New blog post
+# -------------
+
+# Be sure you are in the right directory
+cd 
+
+# Create post file and (empty) image directory
+# Populate post file with front matter
+
+postdate="2021-01-28"
+postname="$postdate-github-pages"
+
+postfile="_posts/$postname.md"
+touch $postfile
+
+postimgpath=assets/images/$postname
+mkdir -p $postimgpath
+
+echo '---'                                                       >  $postfile
+echo 'layout:     post'                                          >> $postfile
+echo 'title:      "GitHub Pages - Getting started"'              >> $postfile
+echo 'date:       2021-01-28 17:30:00 +0100'                     >> $postfile
+echo 'categories: Git GitHub'                                    >> $postfile
+echo "image:      $postimgpath/foo.png"                          >> $postfile
+echo '---'                                                       >> $postfile
+
+cat $postfile
---
+++ about.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+This is the world-famous **Azure for Trainers Blog**.
+
+Our weekly meeting defaults to Thursday 5:30 pm local time.
+
+Have fun!
